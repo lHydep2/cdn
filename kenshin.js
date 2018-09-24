53cd5c5af390570b4cf25d6572f7adc60976ba1b
lHydep2 <43541566+lHydep2@users.noreply.github.com>
2018-09-24 10:03:04 -0300

    Create kenshin.js

---
+++ kenshin.js
@@ -0,0 +1,32 @@
+/**KENSHIN**/
+$(document).ready(function(e){
+ $('.btn_menu-kenshin').click(function( ){
+   $('#menu-kenshin').fadeIn(500)
+ })
+ 
+ $('.fechar').click(function(){
+   $('#menu-kenshin').fadeOut(500)
+ }); 
+});
+
+/**RYOMA**/
+$(document).ready(function(e){
+ $('.btn_menu-ryoma').click(function( ){
+   $('#menu-ryoma').fadeIn(500)
+ })
+ 
+ $('.fechar').click(function(){
+   $('#menu-ryoma').fadeOut(500)
+ }); 
+});
+
+/**ALEK**/
+$(document).ready(function(e){
+ $('.btn_menu-alek').click(function( ){
+   $('#menu-alek').fadeIn(500)
+ })
+ 
+ $('.fechar').click(function(){
+   $('#menu-alek').fadeOut(500)
+ }); 
+});
